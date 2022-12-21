ab0e4b29951d5bfb302d4db48d02c406fbf2e31d
a1tsv <forakk01@yandex.ru>
2022-12-21 13:19:46 +0300

    register part

--- src/features/Register/Register.tsx
+++ src/features/Register/Register.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { useFormik } from "formik";
+import { Grid, Typography } from "@mui/material";
+
+export interface IRegisterFormErrors {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 export const Register = () => {
-  return <div>Register</div>;
+  const registerForm = useFormik({
+    validate: (values) => {
+      const errors = {} as IRegisterFormErrors;
+      if (values.password.length < 8) errors.password = "Incorrect password";
+      if (values.confirmPassword !== values.password)
+        errors.confirmPassword = "Different password";
+      if (!values.email.length) errors.email = "Field is required";
+    },
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <Grid container justifyContent={"center"} alignContent={"center"}>
+      <Grid item>
+        <Typography variant={"h2"}>Sign up</Typography>
+        <form onSubmit={registerForm.handleSubmit}></form>
+      </Grid>
+    </Grid>
+  );
 };
