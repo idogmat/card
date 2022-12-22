b9a32e70d9f727ce0dc8ea14c787fcbca8488fba
a1tsv <forakk01@yandex.ru>
2022-12-22 11:49:10 +0300

    finished register form

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from "./app/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import "./common/styles/null.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
