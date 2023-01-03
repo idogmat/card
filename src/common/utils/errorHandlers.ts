474cd36f8d5c57511bb228a745459e79c1360db3
a1tsv <forakk01@yandex.ru>
2023-01-03 11:11:42 +0300

    added thunkErrorHandler

--- src/common/utils/errorHandlers.ts
+++ src/common/utils/errorHandlers.ts
@@ -1,3 +1,5 @@
+import { AppAC } from "./../../app/appReducer";
+import { AxiosError } from "axios";
 import { FormikProps } from "formik";
 
 export const defaultErrorMessage = "Some errors occurred";
@@ -5,3 +7,16 @@ export const defaultErrorMessage = "Some errors occurred";
 export function hasError(form: FormikProps<any>, prop: string): boolean {
   return !!form.errors[prop] && !!form.touched[prop];
 }
+
+export const errorHandlingThunk = async (thunkAPI: any, logic: Function) => {
+  thunkAPI.dispatch(AppAC.setIsLoading({ isLoading: true }));
+  try {
+    return await logic();
+  } catch (e: any) {
+    const error = e.response ? e.response.data.error : e.message;
+    thunkAPI.dispatch(AppAC.setError({ error: e }));
+    return thunkAPI.rejectWithValue({ error });
+  } finally {
+    thunkAPI.dispatch(AppAC.setIsLoading({ isLoading: false }));
+  }
+};
