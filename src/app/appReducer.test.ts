b463f03309554b11e03e428e06af1cdf6caf1016
a1tsv <altsvbox@gmail.com>
2023-01-11 12:49:08 +0300

    added tests for app extra reduces

--- src/app/appReducer.test.ts
+++ src/app/appReducer.test.ts
@@ -1,24 +1,42 @@
 import { AppAC, appInitialState, appReducer } from "./appSlice";
 
+import { initAppTC } from "./appThunks";
+
 describe("app reducer", () => {
   test("isLoading should be set to true", () => {
     const finalState = appReducer(
       appInitialState,
       AppAC.setIsLoading({ isLoading: true })
     );
+
     expect(finalState.isLoading).toBe(true);
   });
   test("error should be set to string", () => {
     const error = "error placeholder";
+
     const finalState = appReducer(appInitialState, AppAC.setError({ error }));
+
     expect(finalState.error).toBe(error);
   });
   test("successMessage should be set to string", () => {
     const message = "error placeholder";
+
     const finalState = appReducer(
       appInitialState,
       AppAC.setSuccessMessage({ message })
     );
+
     expect(finalState.successMessage).toBe(message);
+  });
+});
+
+describe("app extra reducers", () => {
+  test("isInit should be set with 'initAppTC.fulfilled'", () => {
+    const finalState = appReducer(
+      appInitialState,
+      initAppTC.fulfilled({ isInit: true }, "")
+    );
+
+    expect(finalState.isInit).toEqual(true);
   });
 });
--- src/app/appThunks.ts
+++ src/app/appThunks.ts
@@ -1,6 +1,5 @@
 import { AuthMeTC } from "../features/Auth/authThunks";
 import { createAppAsyncThunk } from "common/utils/AsyncThunk";
-import { errorHandlingThunk } from "common/utils/errorHandlers";
 
 export const initAppTC = createAppAsyncThunk(
   "app/init",
